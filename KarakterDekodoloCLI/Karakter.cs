82d8649d6b755aa4bf9c9b33f8fa07220d2ef5e4
JuhaszZoltan <JuhaszZoltanMate@yahoo.com>
2023-10-06 19:06:52 +0200

    Add project files.

---
+++ KarakterDekodoloCLI/Karakter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarakterDekodoloCLI
+{
+    internal class Karakter
+    {
+        public char Betu { get; set; }
+        public bool[,] Matrix { get; set; }
+
+        public void Megjelenit()
+        {
+            Console.Write('\n');
+            for (int s = 0; s < Matrix.GetLength(0); s++)
+            {
+                for (int o = 0; o < Matrix.GetLength(1); o++)
+                {
+                    Console.Write(Matrix[s, o] ? "X" : " ");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        public bool Felismer(Karakter felism)
+        {
+            for (int s = 0; s < Matrix.GetLength(0); s++)
+            {
+                for (int o = 0; o < Matrix.GetLength(1); o++)
+                {
+                    if (this.Matrix[s, o] != felism.Matrix[s, o]) return false;
+                }
+            }
+            return true;
+        }
+
+        public Karakter(char betu, bool[,] matrix)
+        {
+            Betu = betu;
+            Matrix = matrix;
+        }
+    }
+}
---
+++ KarakterDekodoloCLI/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace KarakterDekodoloCLI
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var bank = Beolvas("bank.txt");
+            Console.WriteLine($"5. feladat: Karakterek száma: {bank.Count}");
+
+            char bekert;
+            bool siker;
+            do
+            {
+                Console.Write("6. feladat: Kérek egy angol nagybetűt: ");
+                siker = char.TryParse(Console.ReadLine(), out bekert);
+            } while (!siker || bekert < 65 || bekert > 90);
+
+            Console.Write("7. feladat: ");
+            var mgj = bank.SingleOrDefault(k => k.Betu == bekert);
+            if (mgj is null) Console.WriteLine("Nincs ilyen karakter a bankban!");
+            else mgj.Megjelenit();
+
+            var dekodol = Beolvas("dekodol.txt");
+
+            Console.WriteLine("9. feladat:");
+            foreach (var dk in dekodol)
+            {
+                var bk = bank.SingleOrDefault(k => k.Felismer(dk));
+                Console.Write(bk is null ? '?' : bk.Betu);
+            }
+        }
+
+        static List<Karakter> Beolvas(string fileNeve)
+        {
+            List<Karakter> karakterek = new();
+            using StreamReader sr = new(
+                @$"..\..\..\src\{fileNeve}",
+                Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                char betu = char.Parse(sr.ReadLine());
+                var matrix = new bool[7, 4];
+                for (int s = 0; s < matrix.GetLength(0); s++)
+                {
+                    string sor = sr.ReadLine();
+                    for (int o = 0; o < sor.Length; o++)
+                    {
+                        matrix[s, o] = sor[o] == '1';
+                    }
+                }
+                karakterek.Add(new(betu, matrix));
+            }
+            return karakterek;
+        }
+    }
+}
